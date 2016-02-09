f78ec8574b48fe49a3abebd9385481e95fb9b1e8
sasaki526 <are0you_zhuge0kongming_desuka@hotmail.co.jp>
2016-02-10 02:50:43 +0900

    add play list feature

---
+++ MobileRadioStation/src/com/mobilestation/mobileradiostation/controllers/PlayListAdapter.java
@@ -0,0 +1,7 @@
+package com.mobilestation.mobileradiostation.controllers;
+
+/**
+ * Created by miyamo on 2016/01/31.
+ */
+public class PlayListAdapter {
+}
---
+++ MobileRadioStation/src/com/mobilestation/mobileradiostation/models/SongInfo.java
@@ -0,0 +1,7 @@
+package com.mobilestation.mobileradiostation.models;
+
+/**
+ * Created by miyamo on 2016/02/06.
+ */
+public class SongInfo {
+}
